c3f3f7d18624c88c7be809532fcd02ab7356c923
Hugh Rawlinson <hugh@codeoclock.net>
2014-03-22 19:14:34 +0000

    Update medida.rb

--- app/models/medida.rb
+++ app/models/medida.rb
@@ -1,6 +1,6 @@
 class Medida < ActiveRecord::Base
   belongs_to :Sensor
-validates_presence_of :nombre, :message=>"Gimme a Name Nigga"
+validates_presence_of :nombre, :message=>"Gimme a Name"
 validates_uniqueness_of :nombre, :message=>"Name already exists" 
 validates_format_of :nombre, :with => /[a-zA-Z0-9_-]+/, :message=>"Invalid Name Format"
 	def to_label
