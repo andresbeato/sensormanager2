0d4130ce5a3448d543bafc4f6af504206e1af209
Hugh Rawlinson <hugh@codeoclock.net>
2014-03-22 19:14:52 +0000

    Update sensor.rb

--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -1,6 +1,6 @@
 class Sensor < ActiveRecord::Base
   belongs_to :Concentrador
- validates_presence_of :nombre, :message=>"Gimme a Name Nigga"
+ validates_presence_of :nombre, :message=>"Gimme a Name"
  validates_uniqueness_of :nombre, :message=>"Name already exists"
  validates_format_of :nombre, :with => /[a-zA-Z0-9_-]+/, :message=>"Invalid Name Format"
 	def to_label
